aea555a721d701ec3b81c41c3d59b54dc28ced23
Dod-o <estyledodo@qq.com>
2024-04-25 15:36:03 +0800

    first commit

--- infinibatch/__init__.py
+++ infinibatch/__init__.py
@@ -1,5 +1,4 @@
-# InfiniBatch
-
+"""
 Infinibatch is a library of checkpointable iterators for randomized data loading of massive data sets in deep neural network training.
 
 
@@ -19,20 +18,10 @@ Infinibatch is a library of checkpointable iterators for randomized data loading
 
 ## Getting Started
 
-Infinibatch requires Python 3.6 or higher and has no dependencies.
+Infinibatch requires Python 3.5 and has no dependencies.
 There is presently no pip package.
 
-To install it, clone this repository and install it locally.
-
-```bash
-git clone https://github.com/microsoft/infinibatch
-cd infinibatch
-pip install -e .
-```
-
-## Documentation
-
-The documentation can be found here: https://microsoft.github.io/infinibatch/
+To install it, see README.md
 
 ## Tutorial
 
@@ -286,51 +275,6 @@ Not all real-life scenarios are covered by this function. For example, multi-tas
 scenarios require more complex combinations of data. To create those, you will need
 to compose the necessary data reader from the underlying building blocks.
 This is described at the documentation of the module `iterators`.
+"""
 
-## Documentation
-
-To view the documentation, please clone the repository and go to docs/infinibatch/index.html
-
-When working on the documentation, install pdoc:
-```
-pip install pdoc3
-```
-You can then start a local http server that dynamically updates the documentation:
-```
-pdoc --template-dir docs --http : infinibatch
-```
-
-We currently haven't set up the CI to automatically generate the documentation.
-Before you merge anything into master, please delete the existing documentation in docs/infinibatch and run
-```
-pdoc -o docs --template-dir docs --html infinibatch
-```
-
-## Testing
-
-To run unit tests, run the following command.
-```
-python -m unittest discover -s test
-```
-If you would like the unit tests to stop after the first failed test, use:
-```
-python -m unittest discover -s test --failfast
-```
-To type-check with `mypy` (if installed):
-```
-mypy infinibatch
-```
-
-# Contributing
-
-This project welcomes contributions and suggestions.  Most contributions require you to agree to a
-Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
-the rights to use your contribution. For details, visit https://cla.opensource.microsoft.com.
-
-When you submit a pull request, a CLA bot will automatically determine whether you need to provide
-a CLA and decorate the PR appropriately (e.g., status check, comment). Simply follow the instructions
-provided by the bot. You will only need to do this once across all repos using our CLA.
-
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
-For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
-contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+from .iterators import *
